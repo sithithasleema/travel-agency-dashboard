b6aeca812e1b6677e43a49c688865bcabdc53acb
sithithasleema <sthaslima@gmail.com>
2025-06-04 10:59:40 +1000

    Routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router"
+
+const AdminLayout = () => {
+  return (
+    <div className="admin-layout">AdminLayout
+      <Outlet />
+      </div>
+  );
+};
+export default AdminLayout;
